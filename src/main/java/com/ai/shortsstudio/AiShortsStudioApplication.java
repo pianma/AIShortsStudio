9745482e5714ebbe18f0d74d6c37d82657277d71
kimgeonwoo <ikkw2040@naver.com>
2025-03-09 16:31:34 +0900

    최초 커밋

---
+++ src/main/java/com/ai/shortsstudio/AiShortsStudioApplication.java
@@ -0,0 +1,13 @@
+package com.ai.shortsstudio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AiShortsStudioApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AiShortsStudioApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/ai/shortsstudio/AiShortsStudioApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ai.shortsstudio;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AiShortsStudioApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
