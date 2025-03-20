ebe88fdaac2fca7b952a7174d992a68c65adaae0
kimgeonwoo <ikkw2040@naver.com>
2025-03-20 12:53:56 +0900

    메인화면 임시 설정

--- src/main/java/com/ai/shortsstudio/apispec/adapter/in/Sample.java
+++ src/main/java/com/ai/shortsstudio/apispec/adapter/in/Sample.java
@@ -1,12 +1,14 @@
 package com.ai.shortsstudio.apispec.adapter.in;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class Sample {
 
-    @RequestMapping("/")
+    @GetMapping("/")
+    @ResponseBody
     public String home() {
         return "Hello Docker World2";
     }
