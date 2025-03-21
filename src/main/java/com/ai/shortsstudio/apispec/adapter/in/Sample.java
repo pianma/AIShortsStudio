770d3e72369265d8e230333aad20e926187d7c4c
kimgeonwoo <ikkw2040@naver.com>
2025-03-21 17:32:32 +0900

    화면 구조 설계 및 로그인 화면 구현

--- src/main/java/com/ai/shortsstudio/apispec/adapter/in/Sample.java
+++ src/main/java/com/ai/shortsstudio/apispec/adapter/in/Sample.java
@@ -3,14 +3,16 @@ package com.ai.shortsstudio.apispec.adapter.in;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 public class Sample {
 
     @GetMapping("/")
     @ResponseBody
-    public String home() {
-        return "자동 배포 테스트";
+    public ModelAndView home(ModelAndView model) {
+        model.setViewName("/index");
+        return model;
     }
 
 }
