ad509c2b73df6551d8d3f93f3fcfe4ffe1c882a5
kimgeonwoo <ikkw2040@naver.com>
2025-03-22 15:44:51 +0900

    로그인, 회원가입 화면

--- src/main/java/com/ai/shortsstudio/apispec/adapter/in/Sample.java
+++ src/main/java/com/ai/shortsstudio/apispec/adapter/in/Sample.java
@@ -2,15 +2,19 @@ package com.ai.shortsstudio.apispec.adapter.in;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 public class Sample {
 
-    @GetMapping("/")
+    @GetMapping("/login")
     public ModelAndView home(ModelAndView model) {
-        model.setViewName("index");
+        model.setViewName("/auth/login");
+        return model;
+    }
+    @GetMapping("/signup")
+    public ModelAndView signup(ModelAndView model) {
+        model.setViewName("/auth/signup");
         return model;
     }
 
